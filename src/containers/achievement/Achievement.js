89492f4986c991c9c537a86480e0299762760803
Alvaro Sivila <juniorsivila123@gmail.com>
2024-06-19 12:20:09 -0400

    Fix

--- src/containers/achievement/Achievement.js
+++ src/containers/achievement/Achievement.js
@@ -1,14 +1,17 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import "./Achievement.scss";
 import AchievementCard from "../../components/achievementCard/AchievementCard";
-import {achievementSection} from "../../portfolio";
-import {Fade} from "react-reveal";
+import { achievementSection } from "../../portfolio";
+import { Fade } from "react-reveal";
 import StyleContext from "../../contexts/StyleContext";
+
 export default function Achievement() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
+
   if (!achievementSection.display) {
     return null;
   }
+
   return (
     <Fade bottom duration={1000} distance="20px">
       <div className="main" id="achievements">
